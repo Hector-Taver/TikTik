6eb4369f2914c23928fd7e082c047297d8c0433d
Hector van Tol Taver <hectorvttaver@gmail.com>
2022-10-07 18:59:29 -0300

    part 2 - Finished Project

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import { useState, useEffect, FormEvent } from "react";
 import Image from "next/image";
 import Link from "next/link";
 import { useRouter } from "next/router";
@@ -13,7 +13,24 @@ import { createOrGetUser } from "../utils";
 import { useAuthStore } from "../store/authStore";
 
 export const Navbar = () => {
-  const { userProfile, addUser, removeUser } : {userProfile: any, addUser: any, removeUser: any} = useAuthStore();
+  const {
+    userProfile,
+    addUser,
+    removeUser,
+  }: { userProfile: any; addUser: any; removeUser: any } = useAuthStore();
+
+  const [searchValue, setSearchValue] = useState("");
+  const router = useRouter();
+
+  const handleSearch = (e: FormEvent) => {
+    e.preventDefault();
+
+    if (searchValue) {
+      router.push(`/search/${searchValue}`);
+    }
+  };
+
+  useEffect(() => {}, []);
 
   return (
     <div className="w-full flex justify-between items-center border-b-2 border-gray-200 py-2 px-4">
@@ -28,7 +45,27 @@ export const Navbar = () => {
         </div>
       </Link>
 
-      <div>SEARCH</div>
+      <div className="relative hidden md:block">
+        <form
+          onSubmit={handleSearch}
+          className="absolute md:static top-10 left-10 bg-white"
+        >
+          <input
+            type="text"
+            value={searchValue}
+            onChange={(e) => setSearchValue(e.target.value)}
+            placeholder="Search accounts and videos"
+            className="bg-primary p-3 md:text-md font-medium border-2 border-gray-100 focus:outline-none focus:border-2 focus:border-gray-300 rounded-full w-[300px] md:w-[350px] md:top-0"
+          />
+          <button
+            onClick={handleSearch}
+            className="absolute md:right-5 right-6 top-4 border-l-2 border-gray-300 pl-4 text-2xl text-gray-400 hover:brightness-90 transition-colors"
+          >
+            <BiSearch />
+          </button>
+        </form>
+      </div>
+
       <div>
         {userProfile ? (
           <div className="flex gap-5 md:gap-10">
--- components/NoResults.tsx
+++ components/NoResults.tsx
@@ -1,12 +1,21 @@
-import { NextPage } from 'next'
-import React from 'react'
+import { MdOutlineVideocamOff } from "react-icons/md";
+import { BiCommentX } from "react-icons/bi";
 
 type IProps = {
-  text: string
-}
+  text: string;
+};
 
-export const NoResults: NextPage<IProps> = ({text}) => {
+export const NoResults = ({ text }: IProps) => {
   return (
-    <div>NoResults</div>
-  )
-}
+    <div className="flex flex-col justify-center items-center h-full w-full">
+      <p className="text-8xl">
+        {text === "No comments yet." ? (
+          <BiCommentX />
+        ) : (
+          <MdOutlineVideocamOff />
+        )}
+      </p>
+      <p className="text-2xl text-center">{text}</p>
+    </div>
+  );
+};
